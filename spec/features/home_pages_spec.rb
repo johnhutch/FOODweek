4888b526dc2a05c800928e2f472aecc2078798a4
jon-athan-hall <jonathan.edward.hall@gmail.com>
2016-10-22 13:15:29 -0400

    db schema file update and spec test

--- spec/features/home_pages_spec.rb
+++ spec/features/home_pages_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.feature "home page", type: :feature do
   describe "navigation" do 
-    it " shows me the menu" do 
+    it "shows me the menu" do 
       visit "/"
       expect(page.find('.navbar')).to have_content("FOODweek") # page find search for css path
     end
