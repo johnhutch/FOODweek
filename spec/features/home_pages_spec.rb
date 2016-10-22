911a2b10f36afb3932e53cdadbd398a3d214c5f9
John Hutchinson <johnhutch@gmail.com>
2016-10-22 11:25:59 -0400

    #2 fixed first test, solved first test.

--- spec/features/home_pages_spec.rb
+++ spec/features/home_pages_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature "home page", type: :feature do
   describe "navigation" do 
     it " shows me the menu" do 
       visit "/"
-      expect(page.find('.nav-bar')).to have_content("brand") # page find search for css path
+      expect(page.find('.navbar')).to have_content("FOODweek") # page find search for css path
     end
   end
 end
