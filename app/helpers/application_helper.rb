315e4b5e3d36a24671346a73be1182c1581fe5d9
jon-athan-hall <jonathan.edward.hall@gmail.com>
2016-10-22 16:10:18 -0400

    overhaul of markup for all recipes views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+  def markdown(text)
+    # Settings for the markdown.render method to use.
+    renderer = Redcarpet::Render::HTML.new(
+      filter_html: true,
+      no_images: true,
+      no_links: true,
+      no_styles: true,
+      hard_wrap: true,
+      prettify: true,
+    )
+
+    markdown = Redcarpet::Markdown.new(
+      renderer,
+      no_intra_emphasis: true,
+      strikethrough: true,
+      lax_spacing: true
+    )
+
+    markdown.render(text).html_safe
+  end
 end
