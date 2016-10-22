f26317684f0861ac60d796bc71d900c6030b620f
John Hutchinson <johnhutch@gmail.com>
2016-10-22 10:47:27 -0400

    #33 -- created MealPlan and created HABTM association with recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,7 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+  has_and_belongs_to_many :meal_plans
+
   validates :name, presence: true
   validates :ingredients, presence: true
   validates :steps, presence: true
