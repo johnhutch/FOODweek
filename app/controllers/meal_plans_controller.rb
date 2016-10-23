ab1645d56fba10a8ef06a1c3eb58e6dd185b2636
jon-athan-hall <jonathan.edward.hall@gmail.com>
2016-10-23 00:52:49 -0400

    final markup clean up for dashboard#user, i don't know why i started doing this, but it's all set for sammy now, still working on the add to meal plan feature

--- app/controllers/meal_plans_controller.rb
+++ app/controllers/meal_plans_controller.rb
@@ -71,6 +71,9 @@ class MealPlansController < ApplicationController
     end
   end
 
+  def remove_recipe
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_meal_plan
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   get "recipes/mine", to: "recipes#mine"
   resources :recipes
 
-  get "dashboard", to: "dashboard#user_dash"
+  get "dashboard", to: "dashboard#user"
 
+  post "add_recipe_to_meal_plan/:id", to: "meal_plan#add_recipe"
+  post "remove_recipe_from_meal_plan/:id", to: "meal_plan#remove_recipe"
   resources :meal_plans
 end
