5323147a26e466a39bb41ae307fcb3cb4844c943
johnhutch <johnhutch@gmail.com>
2016-10-23 03:44:14 -0400

    fixed bug on new meal plan page, wrote test to verify new meal plan creation

--- spec/features/meal_plans_spec.rb
+++ spec/features/meal_plans_spec.rb
@@ -13,4 +13,19 @@ RSpec.feature "Meal Plan", type: :feature do
       page.has_css?('div.no_recipes')
     end
   end
+
+  describe "GET /meal_plans/new" do
+    it " it creates a new meal_plan via the new meal_plan web form" do
+        login(user1)
+        recipe
+
+        visit dashboard_path
+        page.has_css?('li.recipe-briefs__list-item')
+        click_link('new_meal_plan')
+        fill_in "Name", :with => "My new meal plan"
+        check("meal_plan_recipe_ids_" + recipe.id.to_s)
+        click_button('submit_meal_plan')
+        page.has_css?('li.dashboard__ingredients_set')
+    end
+  end
 end
