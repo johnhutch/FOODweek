fd3169c5b559c0ff916482fb67020e582ab88015
johnhutch <johnhutch@gmail.com>
2016-10-30 09:36:54 -0400

    cleaning up tests and page language

--- spec/features/home_pages_spec.rb
+++ spec/features/home_pages_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature "home page", type: :feature do
   describe "homepage" do 
     it " shows up because we haven't completely destroyed the site" do 
       visit "/"
-      expect(page.find('header')).to have_content("FOODweek") # page find search for css path
+      expect(page.find('header')).to have_content( I18n.t('brand') ) # page find search for css path
     end
   end
 end
--- spec/features/meal_plans_spec.rb
+++ spec/features/meal_plans_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.feature "Meal Plan", type: :feature do
   let(:user1) {FactoryGirl.create(:user)}
   let(:recipe) { FactoryGirl.create(:recipe, user: user1) }
-  let(:mealplan1) { FactoryGirl.create(:meal_plan, user_id: user1.id) }
+  let(:mealplan1) { FactoryGirl.create(:meal_plan, user: user1) }
 
   describe "GET /meal_plans/1" do
     it " tells you you don't have any meals in your meal plan" do 
       login(user1)
 
       visit meal_plan_path(mealplan1)
-      page.has_css?('div.no_recipes')
+      expect(page).to have_selector('.test-no_recipes')
     end
   end
 
