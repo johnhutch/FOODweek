9769324c4f2681090915ffcb4131ed8b3bc0e9f5
johnhutch <johnhutch@gmail.com>
2016-11-02 00:21:26 -0400

    added email reset test. issue #2

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -32,4 +32,45 @@ RSpec.feature "User Dashboard", type: :feature do
       expect(page).to have_content( I18n.t('devise.registrations.signed_up') )
     end
   end
+
+
+  describe "user1 password recovery" do
+  #   As a user who forgot my password
+  #   I want to recover my site access easily
+  # 
+    specify "email recovery of a new password" do
+      user1
+      visit new_user_session_path
+      click_on I18n.t('devise.shared_links.forgot_password')
+      fill_in "Email", :with => user1.email
+      click_on I18n.t('devise.passwords.send_reset')
+
+      original_password = user1.password
+      current_email_address = user1.email
+      # EmailSpec::EmailViewer::save_and_open_all_raw_emails
+      expect(unread_emails_for(user1.email)).to be_present
+      open_email user1.email, :with_subject => "Reset password instructions"
+      click_first_link_in_email
+      expect(page).to have_selector(".test--change_password")
+      fill_in "user_password", :with => "newpass"
+      fill_in "user_password_confirmation", :with => "newpass"
+      click_button I18n.t('devise.passwords.submit_button')
+      expect(page).to have_content I18n.t('devise.passwords.updated') 
+      click_link I18n.t('devise.nav.log_out')
+
+      # i should not be able to log in with the old password
+      visit new_user_session_path
+      fill_in "user_email", :with => user1.email
+      fill_in "user_password", :with =>  original_password
+      click_button "Log in"
+      expect(page).to have_content I18n.t('devise.failure.invalid', authentication_keys: "Email")
+
+      # I should be able to log in with the new password
+      visit new_user_session_path
+      fill_in "user_email", :with => user1.email
+      fill_in "user_password", :with => "newpass"
+      click_button "Log in"
+      expect(page).to have_content I18n.t('devise.sessions.signed_in')
+    end
+  end
 end
