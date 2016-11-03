dad16596b9a1781531acefc80f5d85954e713577
johnhutch <johnhutch@gmail.com>
2016-11-02 22:34:37 -0400

    fixed indenting

--- spec/features/recipes_spec.rb
+++ spec/features/recipes_spec.rb
@@ -17,19 +17,19 @@ RSpec.feature "Recipe", type: :feature do
 
   describe "GET /recipes/new" do
     it " it creates a new recipe via the recipe meal_plan web form" do
-        login(user1)
+      login(user1)
 
-        visit dashboard_path
-        click_link I18n.t('dashboard.new_recipe')
-        fill_in "recipe_name", :with => "My new recipe"
-        click_button I18n.t('recipes.submit_new')
-        expect(page).to have_selector('.form-errors__list-item')
-        fill_in "recipe_time", :with => "30"
-        fill_in "recipe_ingredients", :with => "Foo\bbar"
-        fill_in "recipe_steps", :with => "blah blah blah"
-        click_button I18n.t('recipes.submit_new')
-        expect(page).not_to have_selector('.form-errors__list-item')
-        expect(page).to have_content I18n.t('recipes.new_saved')
+      visit dashboard_path
+      click_link I18n.t('dashboard.new_recipe')
+      fill_in "recipe_name", :with => "My new recipe"
+      click_button I18n.t('recipes.submit_new')
+      expect(page).to have_selector('.form-errors__list-item')
+      fill_in "recipe_time", :with => "30"
+      fill_in "recipe_ingredients", :with => "Foo\bbar"
+      fill_in "recipe_steps", :with => "blah blah blah"
+      click_button I18n.t('recipes.submit_new')
+      expect(page).not_to have_selector('.form-errors__list-item')
+      expect(page).to have_content I18n.t('recipes.new_saved')
     end
   end
 end
