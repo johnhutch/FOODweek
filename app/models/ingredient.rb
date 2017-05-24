866486202ed4ce6669bdf7e0b31de6a3f07ec2d3
johnhutch <johnhutch@gmail.com>
2017-05-24 19:55:17 -0400

    fixed indenting on ingredient.rb

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,17 +1,17 @@
 class Ingredient < ApplicationRecord
 
   def numeric_amount
-      # because "2".to_r == 2/1, and no one writes recipes like that
-      # so we check to see if the denominator is 1. if so, use to_i. otherwise, .to_r
-      if self.amount
-        self.amount.to_r.denominator == 1 ? self.amount.to_i : self.amount.to_r
-      else
-        ""
-      end
+    # because "2".to_r == 2/1, and no one writes recipes like that
+    # so we check to see if the denominator is 1. if so, use to_i. otherwise, .to_r
+    if self.amount
+      self.amount.to_r.denominator == 1 ? self.amount.to_i : self.amount.to_r
+    else
+      ""
+    end
   end
 
   def unitized_amount
-      Unit.new(self.amount + (self.unit || "") )
+    Unit.new(self.amount + (self.unit || "") )
   end
 
   def ingreedy_parse(ing_string)
