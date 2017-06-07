e7d03b65bb095cd1f4cf1eda8415709bb19723d4
johnhutch <johnhutch@gmail.com>
2017-06-06 23:02:54 -0400

    issue 112 - bug discovered whilst working on #112, need to check for compatible units before adding/subtracting using unit1.compatible?(unit2)

--- spec/models/meal_plan_spec.rb
+++ spec/models/meal_plan_spec.rb
@@ -9,6 +9,9 @@ RSpec.describe GroceryList, type: :model do
                                      2 cloves garlic, finely chopped
                                      4 cm piece of fresh ginger, peeled and finely chopped
                                      6 1/2 lbs boneless pork shoulder, rind removed|) }
+  let(:recipe2) { FactoryGirl.create(:recipe, user: user1, ingredients_block: %Q|5 tbsp soy sauce
+                                     4 grams mild or hot chili powder
+                                     6 1/2 lbs boneless pork shoulder, rind removed|) }
   let(:mealplan1) { FactoryGirl.create(:meal_plan, user: user1) }
 
   it "doesn't crash when subtracting a recipe with a weird unit" do
@@ -16,5 +19,11 @@ RSpec.describe GroceryList, type: :model do
     mealplan1.recipes.delete(recipe1)
 		expect(mealplan1).to be_valid
 	end
+
+  it "doesn't crash when trying to add incompatible units" do
+    mealplan1.recipes << recipe1
+    mealplan1.recipes << recipe2
+		expect(mealplan1).to be_valid
+  end
 
 end
