1e5481f5d002fb6ff8d9b2b9dc2cbffb97e50e68
John Hutchinson <johnhutch@gmail.com>
2017-06-14 01:41:40 -0400

    Issue #112 - added Pry for debugging, wrote a shit-ton more tests to try to find the source of the remaining failed test. No dice.

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -8,7 +8,8 @@ RSpec.describe Ingredient, type: :model do
   let(:ingredient2) { FactoryGirl.create(:ingredient, parent: recipe2) }
   let(:ingredient3) { FactoryGirl.create(:ingredient, parent: recipe2, amount: 4) }
   let(:ing_liter) { FactoryGirl.create(:ingredient, parent: recipe2, unit: "liter", amount: 4) }
-  let(:ing_unitless) { FactoryGirl.create(:ingredient, parent: recipe2, unit: nil, amount: 4) }
+  let(:ing_unitless1) { FactoryGirl.create(:ingredient, parent: recipe2, unit: nil, amount: 4) }
+  let(:ing_unitless2) { FactoryGirl.create(:ingredient, parent: recipe2, unit: nil, amount: 3) }
 
   it "is valid with valid attributes" do
 		expect(Ingredient.new(name: ingredient1.name, parent: recipe1)).to be_valid
@@ -28,12 +29,17 @@ RSpec.describe Ingredient, type: :model do
   end
 
   it "returns false if trying to add to nil unit'd ingredient to a unit'd ingredient" do
-    expect(ing_unitless.sum(ingredient2)).to eq false
+    expect(ing_unitless1.sum(ingredient2)).to eq false
   end
 
   it "subtracts two compatibly unit'd ingredients" do
     ingredient3.sub(ingredient2)
     expect(ingredient3.unitized_amount.to_s.partition(" ").first).to eq "2.5"
+  end
+
+  it "subtracts two unitless ingredients" do
+    ing_unitless1.sub(ing_unitless2)
+    expect(ing_unitless1.unitized_amount.to_s.partition(" ").first).to eq "1"
   end
 
   it "returns false if trying to subtract equal to or more than the amount available" do
--- spec/models/meal_plan_spec.rb
+++ spec/models/meal_plan_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe GroceryList, type: :model do
+RSpec.describe MealPlan, type: :model do
   let(:user1) {FactoryGirl.create(:user)}
   let(:recipe1) { FactoryGirl.create(:recipe, user: user1, ingredients_block: %Q|5 tbsp soy sauce
                                      4 tbsp mild or hot chili powder
@@ -13,6 +13,10 @@ RSpec.describe GroceryList, type: :model do
                                      4 grams mild or hot chili powder
                                      6 1/2 lbs boneless pork shoulder, rind removed|) }
   let(:recipe3) { FactoryGirl.create(:recipe, user: user1, ingredients_block: %Q|2 cloves garlic, finely chopped| ) }
+  let(:recipe_unitless_1) { FactoryGirl.create(:recipe, user: user1, ingredients_block: "1 apple") }
+  let(:recipe_unitless_2) { FactoryGirl.create(:recipe, user: user1, ingredients_block: "3 apples") }
+  let(:recipe5) { FactoryGirl.create(:recipe, user: user1, ingredients_block: "4 pounds of apples") }
+  let(:recipe6) { FactoryGirl.create(:recipe, user: user1, ingredients_block: "4 grams of apples") }
   let(:mealplan1) { FactoryGirl.create(:meal_plan, user: user1) }
 
   it "doesn't crash when subtracting a recipe with a weird unit" do
@@ -32,6 +36,60 @@ RSpec.describe GroceryList, type: :model do
     mealplan1.recipes << recipe2
     mealplan1.recipes.delete(recipe2)
     expect(mealplan1).to be_valid
+  end
+
+  it "creates a user's grocery list when adding to the mealplan" do
+    mealplan1.recipes << recipe1
+    expect(mealplan1.user.grocery_list).to be_valid
+  end
+
+  it "adds a recipe's ingredients to a user's grocery list when adding a recipe to a user's mealplan" do
+    mealplan1.recipes << recipe1
+    list_ing = mealplan1.user.grocery_list.ingredients.first 
+    expect(list_ing.unitized_amount).to eq recipe1.ingredients.first.unitized_amount
+  end
+
+  it "sums a recipe's ingredients to a user's grocery list when adding a recipe to a user's mealplan" do
+    mealplan1.recipes << recipe1
+    mealplan1.recipes << recipe2
+    list_ing = mealplan1.user.grocery_list.ingredients.first
+    expect(list_ing.amount).to eq (recipe1.ingredients.first.amount.to_i + recipe2.ingredients.first.amount.to_i).to_s
+  end
+
+  it "sums a recipe's unitless ingredients to a user's grocery list when adding a recipe to a user's mealplan" do
+    mealplan1.recipes << recipe_unitless_1
+    mealplan1.recipes << recipe_unitless_2
+    list_ing = mealplan1.user.grocery_list.ingredients.first
+    expect(list_ing.amount).to eq (recipe_unitless_1.ingredients.first.amount.to_i + recipe_unitless_2.ingredients.first.amount.to_i).to_s
+  end
+
+  it "tries to sum a unitless with a unit'd ingredient, but can't, so it just adds the unitless one" do
+    mealplan1.recipes << recipe_unitless_2
+    mealplan1.recipes << recipe5
+    list_ing = mealplan1.user.grocery_list.ingredients.last
+    expect(list_ing.unitized_amount).to eq recipe5.ingredients.first.unitized_amount
+  end
+
+  it "subtracts compatibly unit'd ingredients from a user's grocery list when a recipe is removed from their meal plan." do
+    mealplan1.recipes << recipe5
+    mealplan1.recipes << recipe6
+    mealplan1.recipes.delete(recipe5)
+    expect(mealplan1.user.grocery_list.ingredients.first.unitized_amount).to eq recipe6.ingredients.first.unitized_amount
+  end
+
+  it "subtracts compatibly unitless ingredients from a user's grocery list when a recipe is removed from their meal plan." do
+    mealplan1.recipes << recipe_unitless_1
+    mealplan1.recipes << recipe_unitless_2
+    mealplan1.recipes.delete(recipe_unitless_1)
+    expect(mealplan1.user.grocery_list.ingredients.first.unitized_amount).to eq recipe_unitless_2.ingredients.first.unitized_amount
+  end
+
+  it "skips incompatibly unit'd ingredients on from a user's grocery list when a recipe is removed from their meal plan, and just removes the correct ingredient." do
+    mealplan1.recipes << recipe5
+    mealplan1.recipes << recipe_unitless_1
+    mealplan1.recipes.delete(recipe5)
+    expect(mealplan1.user.grocery_list.ingredients.first.unitized_amount).to eq recipe_unitless_1.ingredients.first.unitized_amount
+    expect(mealplan1.user.grocery_list.ingredients.first.unitized_amount).to_not eq recipe5.ingredients.first.unitized_amount
   end
 
 end
